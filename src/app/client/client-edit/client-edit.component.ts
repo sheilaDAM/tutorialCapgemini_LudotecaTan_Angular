ca4cc99785bc3b202af4ea53a5d28ae5f656fc39
Jiménez Nieto external <sheila-isabel.jimenez-nieto.external@capgemini.com>
2024-06-27 13:33:56 +0200

    Creating error message to client if a name already exists in database using backend to check it and fixing to manage which error show, in case name is empty or already exists.

--- src/app/client/client-edit/client-edit.component.ts
+++ src/app/client/client-edit/client-edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ErrorHandler, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Client } from '../model/Client';
 import { ClientService } from '../client-service/client.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-client-edit',
@@ -11,6 +12,7 @@ import { ClientService } from '../client-service/client.service';
 export class ClientEditComponent implements OnInit {
 
   client: Client;
+  errorMessage: string;
 
   constructor(
     public dialogRef: MatDialogRef<ClientEditComponent>,
@@ -19,18 +21,52 @@ export class ClientEditComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
     if (this.data.client != null) {
       this.client = Object.assign({}, this.data.client);
     }
     else {
       this.client = new Client();
     }
+
+    //this.client = this.data.client ? Object.assign({}, this.data.client) : new Client(); //otra manera de poner lo mismo
   }
 
   onSave() {
-    this.clientService.saveClient(this.client).subscribe(result => {
-      this.dialogRef.close();
+
+    //Para poder mostrar el mensaje de error si no hemos insertado nombre y está vacío
+    if (this.client.name.trim() === '') {
+      (error) => this.errorMessage = error.message
+      return;
+    }
+
+    //solo llamará al backend para guardar si el campo de nombre NO está vacío, si hemos insertado un nombre
+    this.clientService.saveClient(this.client).subscribe({
+      next: () => this.dialogRef.close(),
+      error: (error) => this.errorMessage = error.message //si el nombre ya existe en la bbdd saltará un error
     });
+  }
+  
+  //Para manejar más específicamente el error, en caso de desearlo
+  /*
+    this.clientService.saveClient(this.client).subscribe({
+      next: () => this.dialogRef.close(),
+      error: (error) => {
+        if (error.message.includes('El nombre del cliente ya existe')) {
+          this.errorMessage = 'El nombre del cliente ya existe, por favor inserte otro.';
+        } else {
+          this.errorMessage = 'Hubo un error al guardar el cliente.';
+          console.error('Hubo un error:', error);
+        }
+      }
+    });
+  }
+    */
+    
+
+  //este método lo utilizamos para poder eliminar los mensajes de error si modificamos el text input del nombre
+  onInputChange() {
+    this.errorMessage = null;
   }
 
   onClose() {
--- src/app/client/client-service/client.service.ts
+++ src/app/client/client-service/client.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Client } from '../model/Client';
 
 @Injectable({
@@ -11,21 +12,46 @@ export class ClientService {
   private apiUrl = 'http://localhost:8080/client'; // URL del backend
 
   constructor(private http: HttpClient) {
-   }
+  }
 
-   getClients(): Observable<Client[]> {
+  getClients(): Observable<Client[]> {
     //return of(CATEGORY_DATA);
     return this.http.get<Client[]>(this.apiUrl); // Hacemos la solicitud HTTP GET
   }
 
   saveClient(client: Client): Observable<Client> {
     //let url = 'http://localhost:8080/client';
-      
-    if (client.id != null) this.apiUrl += '/'+ client.id;
-    return this.http.put<Client>(this.apiUrl, client);
+
+    //if (client.id != null) this.apiUrl += '/'+ client.id;
+    //return this.http.put<Client>(this.apiUrl, client);
+
+    const url = client.id ? `${this.apiUrl}/${client.id}` : this.apiUrl;
+    return this.http.put<Client>(url, client).pipe(
+      catchError(this.handleError)
+    );
   }
 
-  deleteClient(idClient : number): Observable<any> {
+  private handleError(error: HttpErrorResponse): Observable<never> {
+
+    if (error.status === 400) {
+      return throwError(() => new Error('El nombre del cliente ya existe, por favor inserte otro.')); //throwError(error) is deprecated, now --> throwError(() => new Error('error')
+
+    } else {
+      return throwError(() => new Error('Hubo un error :('));
+    }
+
+    /*
+    //otra manera
+    let errorMessage = 'Hubo un error :(';
+    if (error.status === 400) {
+      errorMessage = 'El nombre del cliente ya existe, por favor inserte otro.';
+    }
+    return throwError(() => new Error(errorMessage));
+    
+    */
+  }
+
+  deleteClient(idClient: number): Observable<any> {
     return this.http.delete(this.apiUrl + "/" + idClient);
-  }  
+  }
 }
